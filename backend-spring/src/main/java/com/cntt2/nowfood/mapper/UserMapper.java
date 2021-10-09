c37a2fcb18736d7d4c6ee0e11c89408886353c91
Viet Anh <truongvietanh221@gmail.com>
2021-10-10 01:12:26 +0700

    config mapstruct

---
+++ backend-spring/src/main/java/com/cntt2/nowfood/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.cntt2.nowfood.mapper;
+
+import com.cntt2.nowfood.domain.User;
+import com.cntt2.nowfood.dto.user.UserRegisterDto;
+import org.mapstruct.Mapper;
+
+/**
+ * @author Vanh
+ * @version 1.0
+ * @date 10/9/2021 10:32 PM
+ */
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User userRegisterToUser(UserRegisterDto dto);
+    UserRegisterDto toUserRegisterDto(User user);
+}
