d12e88767d15a159261aefed5f4208568e602d8d
Khuat Van Chung <chung.khuat@alpaca.vn>
2021-10-29 13:13:56 +0700

    add front

---
+++ frontend-react/coreui-free-react-admin-template/src/layout/DefaultLayout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { Redirect } from 'react-router'
+
+const DefaultLayout = () => {
+  const isLogin = useSelector((state) => state.isLogin)
+  console.log(isLogin)
+  return isLogin ? (
+    <div>
+      <AppSidebar />
+      <div className="wrapper d-flex flex-column min-vh-100 bg-light">
+        <AppHeader />
+        <div className="body flex-grow-1 px-3">
+          <AppContent />
+        </div>
+        <AppFooter />
+      </div>
+    </div>
+  ) : (
+    <Redirect to="/login" />
+  )
+}
+
+export default DefaultLayout
---
+++ frontend-react/coreui-free-react-admin-template/src/views/pages/login/Login.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Redirect } from 'react-router'
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardGroup,
+  CCol,
+  CContainer,
+  CForm,
+  CFormInput,
+  CInputGroup,
+  CInputGroupText,
+  CRow,
+  CFormFeedback,
+} from '@coreui/react'
+import CIcon from '@coreui/icons-react'
+import { cilLockLocked, cilUser } from '@coreui/icons'
+
+const Login = () => {
+  const dispatch = useDispatch()
+  const isLogin = useSelector((state) => state.isLogin)
+
+  const [userName, setUserName] = useState('')
+  const [passWord, setPassWord] = useState('')
+  const [isAuth, setAuth] = useState(false)
+  const [validated, setValidated] = useState(false)
+  const handleSubmit = (event) => {
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    } else {
+      if (userName === 'admin' && passWord === 'admin') {
+        setAuth(true)
+        dispatch({ type: 'set', isLogin: true })
+      }
+    }
+    setValidated(true)
+  }
+
+  return isLogin ? (
+    <Redirect to="/" />
+  ) : isAuth ? (
+    <Redirect to="/" />
+  ) : (
+    <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
+      <CContainer>
+        <CRow className="justify-content-center">
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard className="p-4">
+                <CCardBody>
+                  <CForm
+                    className="row g-3 needs-validation"
+                    noValidate
+                    validated={validated}
+                    onSubmit={handleSubmit}
+                  >
+                    <h1>Login</h1>
+                    <p className="text-medium-emphasis">Sign In to your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput
+                        required
+                        placeholder="Username"
+                        autoComplete="username"
+                        onChange={(e) => setUserName(e.target.value)}
+                      />
+                      <CFormFeedback invalid>Bạn cần nhập tài khoản</CFormFeedback>
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        required
+                        type="password"
+                        placeholder="Password"
+                        autoComplete="current-password"
+                        onChange={(e) => setPassWord(e.target.value)}
+                      />
+                      <CFormFeedback invalid>Bạn cần nhập mật khẩu</CFormFeedback>
+                    </CInputGroup>
+                    <CRow>
+                      <CCol xs={6}>
+                        <CButton color="primary" className="px-4" type="submit">
+                          Login
+                        </CButton>
+                      </CCol>
+                      <CCol xs={6} className="text-right">
+                        <CButton color="link" className="px-0">
+                          Forgot password?
+                        </CButton>
+                      </CCol>
+                    </CRow>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+                <CCardBody className="text-center">
+                  <div>
+                    <h2>Sign up</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Link to="/register">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        Register Now!
+                      </CButton>
+                    </Link>
+                  </div>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
+          </CCol>
+        </CRow>
+      </CContainer>
+    </div>
+  )
+}
+
+export default Login
