c107061d71a59d4fe5d6f6c07b78b9e0546109e2
Viet Anh <truongvietanh221@gmail.com>
2021-11-10 20:30:31 +0700

    Common api

--- backend-spring/src/main/java/com/cntt2/nowfood/rest/CommonController.java
+++ backend-spring/src/main/java/com/cntt2/nowfood/rest/CommonController.java
@@ -1,18 +1,27 @@
 package com.cntt2.nowfood.rest;
 
-/**
- * @author Vanh
- * @version 1.0
- * @date 11/10/2021 12:34 AM
- */
+import com.cntt2.nowfood.exceptions.MessageEntity;
 import io.swagger.annotations.Api;
-import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Vanh
+ * @version 1.0
+ * @date 11/10/2021 8:22 PM
+ */
 @RestController
 @RequestMapping("/api/common")
 @Api(tags = "Common")
-@RequiredArgsConstructor
 public class CommonController {
+    @PostMapping(value="/initial-data")
+    @PreAuthorize("hasAnyAuthority('ADMIN')")
+    public ResponseEntity<?> initialData() {
+        // Todos fake data;
+        return new ResponseEntity<>(new MessageEntity(200, null), HttpStatus.OK);
+    }
 }
