7a72f0db820a6cd945a92b5e0cbe9750c39f7bf7
Khuat Van Chung <chung.khuat@alpaca.vn>
2021-11-28 12:14:35 +0700

    done pharse 1

---
+++ Client/shopeeFood/scripts/data.js
@@ -0,0 +1,127 @@
+const baseUrl = 'http://localhost:8081'
+
+
+var requestProduct = {
+    categoryId: [],
+    categoryShopId: [],
+    isMain: true,
+    keyword: "",
+    pageIndex: 1,
+    pageSize: 12,
+  }
+
+// call data
+GetCategories();
+GetProduct();
+function GetCategories(){
+    $.ajax({
+        url : `${baseUrl}/api/categories`,
+        type:'get',
+        dataType : 'json',
+        data:{
+            
+        },
+        success : function(res){
+            $('.list-category').append('');
+            res.data.forEach((val,index)=>{
+                let row =  `<li class="pt-1">
+                                <input type="checkbox" name="categoryId" value="${val.id}" onChange="ChangeCategory(this)">
+                                <label for="cream">${val.name}</label>
+                            </li>`;
+                $('.list-category').append(row);
+            })
+        }
+    })
+}
+
+
+
+function GetProduct(){
+    $.ajax({
+        url : `${baseUrl}/api/products/search-adv`,
+        type:'post',
+        dataType : 'json',
+        contentType:'application/json',
+        data: JSON.stringify(requestProduct)
+        ,
+        success : function(res){
+            console.log(res)
+            $('.list-product').html('');
+            res.data.content.forEach((val,index)=>{
+                    let row =  `<div class="col-3 item-food" id="f01">
+                                    <a href="/detail.html?Id=${val.shop.id}&productId=${val.id}" class="restaurent">
+                                        <div class="status-circle"></div>
+                                        <img class="image-food" src="${baseUrl+val.image}" alt="" />
+                                        <div class="intro-food">
+                                            <div class="name-food">
+                                                <span>${val.name}</span>
+                                            </div>
+                                            <p class="info-address" title="59 Khương Đình,Hà Nội">
+                                                ${val.shop.shopAddress}
+                                            </p>
+                                        </div>
+                                    </a>
+                                </div>`;
+                $('.list-product').append(row);
+            })
+
+
+            // phân trang tại đây
+            $('.pagination').html('');            
+            if(res.data.totalPages<=3){
+                for (let index = 1; index <= res.data.totalPages; index++) {
+                    let row  = `<li class="page-item">
+                                    <a class="page-link" onClick="ChangePageIndex(${index})">${index}</a>
+                                </li>`;
+                 
+                    $('.pagination').append(row);            
+                }
+            }
+            else if(requestProduct.pageIndex<=2){
+                for (let index = 1; index <= 3; index++) {
+                    let row  = `<li class="page-item">
+                                    <a class="page-link" onClick="ChangePageIndex(${index})">${index}</a>
+                                </li>`;
+                    $('.pagination').append(row);            
+                }
+            }
+            else if(requestProduct.pageIndex>=res.data.totalPages-3){
+                for (let index = requestProduct.pageIndex; index <= res.data.totalPages; index++) {
+                    let row  = `<li class="page-item">
+                                    <a class="page-link" onClick="ChangePageIndex(${index})">${index}</a>
+                                </li>`;
+                    $('.pagination').append(row);            
+                }
+            }
+            else{
+                for (let index = requestProduct.pageIndex-1; index <= requestProduct.pageIndex+1; index++) {
+                    let row  = `<li class="page-item">
+                                    <a class="page-link" onClick="ChangePageIndex(${index})">${index}</a>
+                                </li>`;
+                    $('.pagination').append(row);            
+                }
+            }
+        }
+    })
+}
+
+
+
+// filter event
+function ChangeCategory(e){
+    let val = $(e).val();
+    if(requestProduct.categoryId.filter(x=>x==val).length==0){
+        requestProduct.categoryId.push(val);
+    }
+    else{
+        requestProduct.categoryId.splice(requestProduct.categoryId.findIndex(x=>x==val),1)
+    }
+    requestProduct.pageIndex = 1;
+    GetProduct();
+}
+
+
+function ChangePageIndex(index){
+    requestProduct.pageIndex = index;
+    GetProduct();
+}
---
+++ Client/shopeeFood/scripts/detail.js
@@ -0,0 +1,87 @@
+const params = new window.URLSearchParams(window.location.search);
+const idShop = params.get('Id');
+const productId = params.get('productId');
+const baseUrl = 'http://localhost:8081'
+
+GetCategoryByShop();
+GetShop();
+function GetShop(){
+    $.ajax({
+        url : `${baseUrl}/api/shops/${idShop}/products`,
+        type:'get',
+        dataType : 'json',
+        contentType:'application/json',
+        data: {},
+        success : function(res){
+            console.log(res);
+            $('#image_product').attr('src',baseUrl+res.data.products.filter(x=>x.id==productId)[0].image)
+            $('#name_shop').text(res.data.shopName);
+            $('#address_shop').text(res.data.shopAddress);
+            $('#time_open').text(new Date(res.data.openTime * 1000).getUTCHours()+"00 - "+new Date(res.data.closeTime * 1000).getUTCHours()+":00");
+            $('#range_price').text(res.data.products.reduce(function(prev, curr) {
+                return prev.price < curr.price ? prev : curr;
+            }).price+"VND - " + 
+            res.data.products.reduce(function(prev, curr) {
+                return prev.price > curr.price ? prev : curr;
+            }).price+"VND"
+            );
+
+
+            // danh sách món ăn
+
+            $('.intro-menu').html('');
+            res.data.products.forEach((val,index)=>{
+                let row = `<div class="item-menu mt-1 d-flex justify-content-between">
+                                <div class="d-flex">
+                                    <img class="item-restaurant-img img-item19833" src="${baseUrl+val.image}" alt="">
+                                    <div class="item-restaurant-name">
+                                        <p class="name-item19833">${val.name}</p>
+                                    </div>
+                                </div>
+                                <div class="price-btn d-flex">
+                                    <div class="food-price">
+                                        <p class="initial-price"><del>${val.price} <sup>đ</sup></del></p>
+                                    </div>
+                                    <button class="btn-order text-center" id="btn-order-item19833" onclick="OrderFood(${val.id})"><i class="fas fa-plus"></i></button>
+                                </div>
+                            </div>`;
+                $('.intro-menu').append(row);
+            })
+        }
+    })
+}
+
+
+function GetCategoryByShop(){
+    $.ajax({
+        url : `${baseUrl}/api/shops/${idShop}/category-shop`,
+        type:'get',
+        dataType : 'json',
+        contentType:'application/json',
+        data: {},
+        success : function(res){
+            $('.list-menu').html('');
+            res.data.forEach((val,index)=>{
+                let row = `<li class="menu-item">
+                                <a href="">${val.name}</a>
+                            </li>`;
+                $('.list-menu').append(row);
+            });
+        }
+    })
+}
+
+
+function OrderFood(id){
+    $.ajax({
+        url : `${baseUrl}/api/products/${id}`,
+        type:'get',
+        dataType : 'json',
+        contentType:'application/json',
+        data: {},
+        success : function(res){
+            console.log(res);
+        }
+    })
+    $('#exampleModalCenter').modal('show');
+}
